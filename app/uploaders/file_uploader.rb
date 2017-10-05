e670d1655061ae7dcfa1e68e9553355dca0e7d6e
Brendan Ryan <brendan.ryan@varland.com>
2017-10-05 09:33:58 -0400

    Fixes Salt Spray Attachment Display Issue

--- app/uploaders/file_uploader.rb
+++ app/uploaders/file_uploader.rb
@@ -2,71 +2,71 @@
 
 class FileUploader < CarrierWave::Uploader::Base
 
-    # Include RMagick or MiniMagick support:
-    # include CarrierWave::RMagick
-    include CarrierWave::MiniMagick
+  # Include RMagick or MiniMagick support:
+  # include CarrierWave::RMagick
+  include CarrierWave::MiniMagick
 
-    process :fix_exif_rotation
-    def fix_exif_rotation
-      if file.content_type == 'image/jpeg'
-        manipulate! do |img|
-          img.tap(&:auto_orient)
-        end
+  process :fix_exif_rotation
+  def fix_exif_rotation
+    if file.content_type == 'image/jpeg'
+      manipulate! do |img|
+        img.tap(&:auto_orient)
       end
     end
-
-    process :save_content_type_in_model
-
-    def save_content_type_in_model
-      model.content_type = file.content_type if file.content_type
-    end
-
-    # Choose what kind of storage to use for this uploader:
-    storage :file
-    # storage :fog
-
-    # Override the directory where uploaded files will be stored.
-    # This is a sensible default for uploaders that are meant to be mounted:
-    def store_dir
-      "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
-    end
-
-    # Provide a default URL as a default if there hasn't been a file uploaded:
-    # def default_url
-    #   # For Rails 3.1+ asset pipeline compatibility:
-    #   # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
-    #
-    #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
-    # end
-
-    # Process files as they are uploaded:
-    # process :scale => [200, 300]
-    #
-    # def scale(width, height)
-    #   # do something
-    # end
-
-    # Create different versions of your uploaded files:
-    version :thumb, if: :is_img? do
-      process :resize_to_fit => [200, 5000]
-    end
-
-    # Add a white list of extensions which are allowed to be uploaded.
-    # For images you might use something like this:
-    def extension_white_list
-      %w(jpg jpeg gif png doc xls docx xlsx pdf mov mp4 tiff)
-    end
-
-    # Override the filename of the uploaded files:
-    # Avoid using model.id or version_name here, see uploader/store.rb for details.
-    # def filename
-    #   "something.jpg" if original_filename
-    # end
-
-  private
-
-    def is_img? file
-      file.content_type.start_with?('image')
-    end
-
   end
+
+  process :save_content_type_in_model
+
+  def save_content_type_in_model
+    model.content_type = file.content_type if file.content_type
+  end
+
+  # Choose what kind of storage to use for this uploader:
+  storage :file
+  # storage :fog
+
+  # Override the directory where uploaded files will be stored.
+  # This is a sensible default for uploaders that are meant to be mounted:
+  def store_dir
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
+
+  # Provide a default URL as a default if there hasn't been a file uploaded:
+  # def default_url
+  #   # For Rails 3.1+ asset pipeline compatibility:
+  #   # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
+  #
+  #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
+  # end
+
+  # Process files as they are uploaded:
+  # process :scale => [200, 300]
+  #
+  # def scale(width, height)
+  #   # do something
+  # end
+
+  # Create different versions of your uploaded files:
+  version :thumb, if: :is_img? do
+    process :resize_to_fit => [200, 5000]
+  end
+
+  # Add a white list of extensions which are allowed to be uploaded.
+  # For images you might use something like this:
+  def extension_white_list
+    %w(jpg jpeg gif png doc xls docx xlsx pdf mov mp4 tiff)
+  end
+
+  # Override the filename of the uploaded files:
+  # Avoid using model.id or version_name here, see uploader/store.rb for details.
+  # def filename
+  #   "something.jpg" if original_filename
+  # end
+
+private
+
+  def is_img? file
+    file.content_type.start_with?('image')
+  end
+
+end
