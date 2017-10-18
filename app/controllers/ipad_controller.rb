da51de1973e9aa334e94004eda6fe79850dbac22
Brendan Ryan <brendan.ryan@varland.com>
2017-10-18 15:19:35 -0400

    Adds Boostrap 3 btn-default Styling

--- app/controllers/ipad_controller.rb
+++ app/controllers/ipad_controller.rb
@@ -2,6 +2,10 @@ class IpadController < ApplicationController
   def index
   end
 
+  def new
+    @employee = User.find_by_employee_number(params[:employee_number])
+  end
+
   def enter_pin
     @employee = User.find_by_employee_number(params[:employee_number])
   end
--- app/controllers/timeclock_records_controller.rb
+++ app/controllers/timeclock_records_controller.rb
@@ -2,9 +2,9 @@ class TimeclockRecordsController < ApplicationController
   before_action :set_timeclock_record, only: [:show, :edit, :update, :destroy]
 
   def index
-    user_view = User.find_by_employee_number(params[:user_view]).id || current_user.id
-    @timeclock_records = TimeclockRecord.where('user_id = ?', user_view)
-    @flagged_records = TimeclockRecord.where('is_flagged = ?', true)
+    @employee = User.find_by_employee_number(params[:user_view]) || current_user
+    @timeclock_records = TimeclockRecord.where('user_id = ?', @employee.id)
+    @flagged_records = TimeclockRecord.where('user_id = ? AND is_flagged = ?', @employee.id, true)
   end
 
   def new
