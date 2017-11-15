4d63b71d73f6575f5be219bb116e10d396a83209
Brendan Ryan <brendan.ryan@varland.com>
2017-11-15 09:34:50 -0500

    Fixes Date/Time Formatting On Flagged Record Email

--- test/mailers/previews/flagged_record_mailer_preview.rb
+++ test/mailers/previews/flagged_record_mailer_preview.rb
@@ -1,6 +1,6 @@
 # Preview all emails at http://localhost:3000/rails/mailers/flagged_record_mailer
 class FlaggedRecordMailerPreview < ActionMailer::Preview
   def flagged_record_preview
-    FlaggedRecordMailer.flagged_record_email(TimeclockRecord.first, 'test')
+    FlaggedRecordMailer.flagged_record_email(TimeclockRecord.find(2), 'test')
   end
 end
