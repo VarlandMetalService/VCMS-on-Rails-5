aff0d227b07fa7dd0e31904d6869cbde25599142
Brendan Ryan <brendan.ryan@varland.com>
2017-12-04 15:31:11 -0500

    Fixes Delete Comment Bug

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,14 +1,20 @@
 class Comment < ApplicationRecord
-  before_validation do
-    self.destroy if self.content && self.content.empty?
-  end
+  before_validation :check_for_blank
 
   belongs_to :commentable, polymorphic: true
   has_many   :attachments,
              as: :attachable,
-             dependent: :destroy
+             dependent: :destroy,
+             after_remove: :check_for_blank
 
   accepts_nested_attributes_for   :attachments,
                                   reject_if: :all_blank,
                                   allow_destroy: true
+
+private
+
+  def check_for_blank(attachment = nil)
+    puts "DEBUG: #{self.content.blank?} : #{self.attachments.size}"
+    self.destroy if self.content.blank? && self.attachments.size == 0
+  end
 end
