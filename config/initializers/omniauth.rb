adb3095f439c5e6ff54c7ed241bd628d11a12da6
Brendan Ryan <brendan.ryan@varland.com>
2018-01-12 12:45:53 -0500

    Fixes Google Authentication Bug

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,7 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'],
+  provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'], skip_jwt: true
     {
       # name: 'name_of_strategy',   # defaults to google_oauth2
       # hd: 'domain.com'            # limits sign-in to a particular Google Apps hosted domain
